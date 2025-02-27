58945cfe215b85d9a2e585f0ddb34e9d1f9bbcae
LeDucLong123 <152180179+LeDucLong123@users.noreply.github.com>
2025-02-27 11:15:35 +0000

    update_1

---
+++ Music/database/migrations/2025_02_27_110022_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('userID');
+            $table->string('roomID');
+            $table->time('time');
+            $table->date('date');
+            $table->boolean('checkout');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+};
